fa85a26cb7d885f442e43d4e0f50fc725a0ef0df
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-27 20:31:03 +0330

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 OWNER_ID = int(os.getenv('OWNER_ID'))
 ADMINS = list(map(int, os.getenv('ADMINS').split()))
-CHANNEL_ID = -1002207109791  # کانالی که ربات فایل نهایی رو اونجا پست میکنه
+CHANNEL_ID = -1002207109791  # کانالی که فایل رو اونجا میفرسته
 REQUIRED_CHANNELS = list(map(int, os.getenv('REQUIRED_CHANNELS').split()))
+ADMIN_FILE = os.getenv('ADMIN_FILE')
