8e3750a7b55a94917c69ed9cf1bc4c2c6c11acc5
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 01:25:46 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-
+BOT_TOKEN = "توکن_ربات_تو"
+OWNER_ID = 123456789  # آی‌دی عددی سازنده
+WEBHOOK_URL = "https://yourprojectname.onrender.com/webhook"
+CHANNEL_ID = -1001234567890  # آیدی عددی کانال (با منفی)
