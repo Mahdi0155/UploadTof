cdae5ef20cdd3e985433e5bd76af636d6fefbdca
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 01:30:24 +0330

    Update subscription.py

--- bot/keyboards/subscription.py
+++ bot/keyboards/subscription.py
@@ -1,1 +1,9 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import CHANNEL_LINK
 
+def subscription_keyboard():
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="عضویت در کانال", url=CHANNEL_LINK)],
+        [InlineKeyboardButton(text="بررسی عضویت ✅", callback_data="check_subscription")]
+    ])
+    return keyboard
