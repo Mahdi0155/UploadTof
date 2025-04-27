662dd812fb894a0aa8cf7e8c77b9f9757b5c4fc7
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-27 23:03:34 +0330

    Update admin_panel.py

--- bot/handlers/admin_panel.py
+++ bot/handlers/admin_panel.py
@@ -1,15 +1,12 @@
-# bot/handlers/admin_panel.py
-
 from aiogram import Router, F
 from aiogram.types import Message
-from config import OWNER_ID
-from bot.keyboards.admin_main import admin_main_menu
+from bot.keyboards.admin import admin_main_menu
+from database.manage_admins import is_owner
 
 router = Router()
 
-@router.message(F.text == "/panel")
-async def open_admin_panel(message: Message):
-    if message.from_user.id != OWNER_ID:
-        await message.answer("شما دسترسی به پنل مدیریت ندارید.")
+@router.message(F.text == "پنل مدیریت")
+async def admin_panel_handler(message: Message):
+    if not await is_owner(message.from_user.id):
         return
-    await message.answer("به پنل مدیریت خوش آمدید:", reply_markup=admin_main_menu())
+    await message.answer("به پنل مدیریت خوش آمدید.", reply_markup=admin_main_menu())
