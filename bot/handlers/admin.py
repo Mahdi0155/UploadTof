7aca67a0ca9e0879dcbd10212f513a7c93d58bad
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-27 23:04:06 +0330

    Update admin.py

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -1,22 +1,22 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from config import OWNER_ID
-from bot.keyboards.admin_keyboards import admin_main_menu
+from bot.keyboards.admin import admin_main_menu
 from database.manage_admins import add_admin, is_owner
 
 router = Router()
 
 @router.message(F.text == "➕ افزودن ادمین جدید")
 async def add_admin_handler(message: Message):
-    if not is_owner(message.from_user.id):
+    if not await is_owner(message.from_user.id):
         return
     await message.answer("لطفا آیدی عددی ادمین جدید را ارسال کنید.")
 
 @router.message(lambda message: message.text.isdigit())
 async def save_admin_id(message: Message):
-    if not is_owner(message.from_user.id):
+    if not await is_owner(message.from_user.id):
         return
-    success = add_admin(int(message.text))
+    success = await add_admin(int(message.text))
     if success:
         await message.answer("✅ ادمین جدید اضافه شد.", reply_markup=admin_main_menu())
     else:
@@ -24,6 +24,6 @@ async def save_admin_id(message: Message):
 
 @router.message(F.text == "🔙 بازگشت به منو اصلی")
 async def back_to_main(message: Message):
-    if not is_owner(message.from_user.id):
+    if not await is_owner(message.from_user.id):
         return
     await message.answer("به منوی اصلی برگشتید.", reply_markup=admin_main_menu())
