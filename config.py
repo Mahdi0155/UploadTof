d186391e63dcb01fbeaea00a6b4900389c75d752
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-27 23:02:59 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,6 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-OWNER_ID = int(os.getenv('OWNER_ID', 0))  # اگر نبود 0 میزاره
-ADMINS = list(map(int, os.getenv('ADMINS', '0').split())) if os.getenv('ADMINS') else []
-CHANNEL_ID = int(os.getenv('CHANNEL_ID', 0))  # اگر نبود 0 میزاره
-ADMIN_FILE = os.getenv('ADMIN_FILE')
-WEBHOOK_URL = "https://uploadtof.onrender.com/webhook"
-
-# پردازش کانال‌های اجباری
-REQUIRED_CHANNELS = os.getenv('REQUIRED_CHANNELS', '').split()
-REQUIRED_CHANNELS = [int(channel) for channel in REQUIRED_CHANNELS if channel]
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
