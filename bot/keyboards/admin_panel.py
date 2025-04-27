baf8f45c3bad27f388b44ecb87b44864f2439aea
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 01:14:40 +0330

    Update admin_panel.py

--- bot/keyboards/admin_panel.py
+++ bot/keyboards/admin_panel.py
@@ -1,10 +1,1 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-def admin_main_menu():
-    keyboard = [
-        [KeyboardButton(text="➕ افزودن ادمین جدید")],
-        [KeyboardButton(text="📁 مدیریت فایل‌ها")],
-        [KeyboardButton(text="👥 مدیریت اعضا")],
-        [KeyboardButton(text="🔙 بازگشت به منو اصلی")],
-    ]
-    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
