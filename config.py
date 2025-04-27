1c6c004a95ea6f8263c18960478a5c89f94b50bb
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-27 21:33:46 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+
 OWNER_ID = int(os.getenv('OWNER_ID'))
-ADMINS = list(map(int, os.getenv('ADMINS').split()))
-CHANNEL_ID = -1002207109791  # کانالی که فایل رو اونجا میفرسته
-REQUIRED_CHANNELS = list(map(int, os.getenv('REQUIRED_CHANNELS').split()))
+
+ADMINS = list(map(int, os.getenv('ADMINS', '').split()))
+
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))  # آیدی چنلی که فایل‌ها اونجا آپلود میشن
+
+REQUIRED_CHANNELS = os.getenv('REQUIRED_CHANNELS', '').split()
+REQUIRED_CHANNELS = [int(channel) for channel in REQUIRED_CHANNELS if channel]
+
 ADMIN_FILE = os.getenv('ADMIN_FILE')
+
 WEBHOOK_URL = "https://uploadtof.onrender.com/webhook"
