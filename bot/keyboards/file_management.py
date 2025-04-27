16c92f4ee055070fef873333b9e9151c4d73c12e
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 01:15:44 +0330

    Delete bot/keyboards/file_management.py

--- bot/keyboards/file_management.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def file_options(file_id):
-    kb = InlineKeyboardMarkup()
-    kb.add(InlineKeyboardButton("❌ حذف فایل", callback_data=f"delete_file:{file_id}"))
-    return kb
