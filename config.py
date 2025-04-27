68d6676949f4efb95cc236ccb70930a7164de976
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-27 19:58:52 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = os.getenv("OWNER_ID")
-REQUIRED_CHANNELS = os.getenv("REQUIRED_CHANNELS", "").split(",")
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+OWNER_ID = int(os.getenv('OWNER_ID'))
+ADMINS = list(map(int, os.getenv('ADMINS').split()))
+CHANNEL_ID = -1002207109791  # کانالی که ربات فایل نهایی رو اونجا پست میکنه
