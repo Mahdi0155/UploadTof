aad83d16ecba5c533ef715857257bad1e9246bac
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 01:15:34 +0330

    Delete bot/keyboards/file_confirmation.py

--- bot/keyboards/file_confirmation.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def confirm_file_keyboard():
-    kb = InlineKeyboardMarkup(row_width=2)
-    kb.add(
-        InlineKeyboardButton(text="✅ تایید و ارسال", callback_data="confirm_send"),
-        InlineKeyboardButton(text="✏️ ویرایش کپشن", callback_data="edit_caption")
-    )
-    return kb
