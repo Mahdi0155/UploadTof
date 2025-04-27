191cd3f891e3b1a3f2f13857685065bf4b7f21e9
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 01:17:44 +0330

    Delete bot/utils directory

--- bot/utils/caption_editor.py
+++
@@ -1,2 +0,0 @@
-def build_caption(caption_text, view_link):
-    return f"{caption_text}\n\n👉 [مشاهده]({view_link})"
--- bot/utils/channel_manager.py
+++
@@ -1,7 +0,0 @@
-from database.manage_subs import add_subscription_channel, remove_subscription_channel
-
-async def add_channel(link):
-    await add_subscription_channel(link)
-
-async def remove_channel(link):
-    await remove_subscription_channel(link)
