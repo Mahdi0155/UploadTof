9b5fdb7c99d9e897032b16ebb01fdbce29fdd871
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 01:28:47 +0330

    Update subscription.py

--- bot/handlers/subscription.py
+++ bot/handlers/subscription.py
@@ -1,1 +1,10 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from database.manage_subscribers import subscribe_user
 
+router = Router()
+
+@router.message(F.text == "/start")
+async def start(message: Message):
+    subscribe_user(message.from_user.id)
+    await message.answer("به ربات خوش آمدید!")
