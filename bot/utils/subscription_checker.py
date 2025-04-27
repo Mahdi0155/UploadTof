e6e5135e43cca5bda106d86d55a8d58657aafa13
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-27 22:01:01 +0330

    Update subscription_checker.py

--- bot/utils/subscription_checker.py
+++ bot/utils/subscription_checker.py
@@ -1,14 +1,12 @@
 from config import REQUIRED_CHANNELS
 from bot.utils.check_user import is_user_in_channel
-from aiogram import Bot
 
-async def is_subscribed(user_id: int, bot: Bot) -> bool:
-    """
-    چک می‌کند آیا کاربر در همه کانال‌های اجباری عضو شده یا نه
-    """
+async def is_subscribed(user_id):
+    from bot.main import bot  # دقت کن اینو اضافه میکنیم
+
     if not REQUIRED_CHANNELS:
-        return True  # اگر کانالی ست نشده بود
-    
+        return True
+
     for channel_id in REQUIRED_CHANNELS:
         if not await is_user_in_channel(bot, user_id, channel_id):
             return False
