4674b200b6262cf8ddaf9ddb741d958557b6c0f5
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-27 22:02:02 +0330

    Update check_subscription.py

--- bot/middlewares/check_subscription.py
+++ bot/middlewares/check_subscription.py
@@ -5,20 +5,14 @@ from bot.utils.check_user import is_user_in_channel
 
 class CheckSubscriptionMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict):
-        # اگر event کاربر نداشت بیخیال شو
         if not hasattr(event, 'from_user') or event.from_user is None:
             return await handler(event, data)
 
-        # اگر لیست کانال های اجباری خالی بود، مستقیم برو ادامه
         if not REQUIRED_CHANNELS:
             return await handler(event, data)
 
-        # چک کردن عضویت کاربر در همه کانال‌ها
         for channel_id in REQUIRED_CHANNELS:
-            is_member = await is_user_in_channel(event.bot, event.from_user.id, channel_id)
-            if not is_member:
-                await event.answer("⚠️ برای استفاده از ربات باید در کانال عضو شوید.")
-                return  # اگر عضو نبود، دیگه ادامه نده
-        
-        # اگر همه اوکی بود، درخواست بره مرحله بعد
+            if not await is_user_in_channel(event.bot, event.from_user.id, channel_id):
+                await event.answer("⚠️ برای استفاده از ربات، لطفا ابتدا در کانال ما عضو شوید.")
+                return  # جلوشو میگیریم که دستور اجرا نشه
         await handler(event, data)
