053111025b6de1f60aef6a1eaaeb3ea6756492b6
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-27 21:54:57 +0330

    Update subscription_checker.py

--- bot/utils/subscription_checker.py
+++ bot/utils/subscription_checker.py
@@ -1,13 +1,13 @@
-from database.manage_subs import get_subscription_channels
+from aiogram import Bot
 from aiogram.exceptions import TelegramBadRequest
 
-async def is_subscribed(user_id):
-    channels = await get_subscription_channels()
-    for channel_id in channels:
-        try:
-            member = await bot.get_chat_member(channel_id, user_id)
-            if member.status not in ["member", "administrator", "creator"]:
-                return False
-        except TelegramBadRequest:
-            return False
-    return True
+async def is_user_in_channel(bot: Bot, user_id: int, channel_id: int) -> bool:
+    """
+    چک می‌کند آیا کاربر در کانال عضو است یا خیر
+    """
+    try:
+        member = await bot.get_chat_member(chat_id=channel_id, user_id=user_id)
+        return member.status in ("member", "administrator", "creator")
+    except TelegramBadRequest:
+        # مثلا کاربر بلاک کرده یا چت پیدا نشده
+        return False
