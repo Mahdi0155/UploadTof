e8dbb4695bf44ad4d4bb4018cc7ed229cef9fa9f
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-27 21:25:01 +0330

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ ADMINS = list(map(int, os.getenv('ADMINS').split()))
 CHANNEL_ID = -1002207109791  # کانالی که فایل رو اونجا میفرسته
 REQUIRED_CHANNELS = list(map(int, os.getenv('REQUIRED_CHANNELS').split()))
 ADMIN_FILE = os.getenv('ADMIN_FILE')
+WEBHOOK_URL = "https://uploadtof.onrender.com/webhook"
